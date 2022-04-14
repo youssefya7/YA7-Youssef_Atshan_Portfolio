c31099a8c16f4bec256ad3e46f90a9228e455d54
Youssef Atshan <engr.youssefya7@gmail.com>
2022-04-14 18:05:45 -0400

    first commit

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export const products = [
     {
       id: 1,
       img: "https://picsum.photos/200/300?random=1",
-      link: "http://lama.dev",
+      link: "https://picsum.photos/200/300?random=1",
     },
     {
       id: 2,
       img: "https://picsum.photos/200/300?random=2",
-      link: "http://lama.dev",
+      link: "https://picsum.photos/200/300?random=2",
     },
     {
       id: 3,
       img: "https://picsum.photos/200/300?random=3",
-      link: "http://lama.dev",
+      link: "https://picsum.photos/200/300?random=3",
     },
     {
       id: 4,
       img: "https://picsum.photos/200/300?random=4",
-      link: "http://lama.dev",
+      link: "https://picsum.photos/200/300?random=4",
     },
     {
       id: 5,
       img: "https://picsum.photos/200/300?random=5",
-      link: "http://lama.dev",
+      link: "https://picsum.photos/200/300?random=5",
     },
     {
       id: 6,
       img: "https://picsum.photos/200/300?random=6",
-      link: "http://lama.dev",
+      link: "https://picsum.photos/200/300?random=6",
     },
   ];
